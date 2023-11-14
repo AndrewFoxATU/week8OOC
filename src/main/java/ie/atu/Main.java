6c0c56cac41cb16054b3e7b7c309a8f08e040b3c
AndrewFoxATU <G00417373@atu.ie>
2023-11-14 13:59:13 +0000

    while loop

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Please enter a number between 0 - 9 :");
-        Scanner myscan = new Scanner(System.in);
+        while(true) {
+            Scanner myscan = new Scanner(System.in);
 
             String input = myscan.nextLine();
-        try {
-            int myNum = Integer.parseInt(input);
+            try {
+                int myNum = Integer.parseInt(input);
 
-            if (myNum >= 0 && myNum <= 9){
-                System.out.println("You entered " + myNum);
-            }
-            else{
-                System.out.println("Not a valid number");
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You entered " + myNum);
+                    break;
+                } else {
+                    System.out.println("Not a valid number (please enter a number between 0 - 9)");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Not a valid character (please enter a number between 0 - 9)");
             }
         }
-        catch(NumberFormatException e){
-            System.out.println("Not a valid character");
-        }
-
     }
 }
