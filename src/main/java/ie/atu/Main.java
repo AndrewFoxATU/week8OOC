1107647c07d5fbdf7694888207b1085dd8ea4940
AndrewFoxATU <G00417373@atu.ie>
2023-11-14 13:32:49 +0000

    main.java

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,19 @@
 package ie.atu;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Please enter a number between 0 - 9 :");
+        Scanner myscan = new Scanner(System.in);
+
+            String input = myscan.nextLine();
+
+            int myNum = Integar.parseInt(input);
+
+            if (myNum >= 0 && myNum <= 9){
+                System.out.println("You entered " + myNum);
+            }
+            else{
+                System.out.println("Not a valid number");
+            }
     }
 }
